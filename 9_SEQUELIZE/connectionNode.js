c290902ba179d790eec46a81205a2dcd4a900524
Renato Alves Soares <renatoguara2020@gmail.com>
2023-02-14 13:17:53 -0300

    ok

--- 9_SEQUELIZE/connectionNode.js
+++ 9_SEQUELIZE/connectionNode.js
@@ -1,17 +1,17 @@
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
+var conn = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "nodesequelize"
 });
 
-con.connect(function(err) {
+export default connect(mapStateToProps, mapDispatchToProps)(containerName).connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
   var sql = "INSERT INTO customers (name, address, email) VALUES ('Company Inc', 'Highway 37','renatoguara2023@gmail.com')";
-  con.query(sql, function (err, result) {
+  conn.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table altered");
   });
